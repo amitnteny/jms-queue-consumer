3affaaf317357f31f26fa7522266bac0a0fafbd7
Amit  Kumar <amit11thstroke@gmail.com>
2018-12-10 01:47:20 +0530

    Project setup

---
+++ src/main/java/com/amitnteny/jmsqueueconsumer/JmsQueueConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.amitnteny.jmsqueueconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsQueueConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsQueueConsumerApplication.class, args);
+	}
+}
